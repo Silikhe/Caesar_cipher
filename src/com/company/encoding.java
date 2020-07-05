e01ae07b6483942bcbabb9c23ba855ce3905ed03
Sil silas <silaswans@gmail.com>
2020-07-05 23:20:54 +0300

    encode

---
+++ src/com/company/encoding.java
@@ -0,0 +1,40 @@
+
+import java.util.Arrays;
+
+public class Encoder {
+
+    private String inputString;
+    private String encodedString;
+    private Character[] alphabeticalLetters = {'A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' } ;
+    private int key = 0;
+
+    public Encoder(String inputString ) {
+        this.inputString = inputString;
+    }
+
+    public String encoderFunc(){
+        String stringPassingEncoding = this.inputString.toUpperCase();
+        int nextCharacterIndex = 0;
+        for (int i = 0; i < stringPassingEncoding.length(); i++) {
+            int characterIndex = Arrays.binarySearch(alphabeticalLetters, stringPassingEncoding.charAt(i));
+            if (characterIndex == -1) {
+                continue;
+            } else{
+                if (characterIndex <= 26-key) {
+                    nextCharacterIndex = characterIndex + key;
+                } else {
+                    nextCharacterIndex = (characterIndex + key) - 26;
+                }
+                char charForEncoding = alphabeticalLetters[nextCharacterIndex];
+                stringPassingEncoding = stringPassingEncoding.substring(0, i)
+                        + charForEncoding + stringPassingEncoding.substring(i + 1);
+            }
+        }
+        return stringPassingEncoding;
+    }
+
+    public String getEncodedString() {
+        this.encodedString = encoderFunc();
+        return this.encodedString;
+    }
+}
