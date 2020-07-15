e1430052e8705836e1539cc0ae9f929923539a75
Sil silas <silaswans@gmail.com>
2020-07-15 23:18:50 +0300

    b

--- src/com/company/encoding.java
+++ src/com/company/encoding.java
@@ -30,3 +30,20 @@ public class App{
        /original statement/ System.out.println("This is Your statement;"+" "+(decodedStatement)+ " The decoded version!!");
     }
 }
+
+public class NumberDigits {
+
+    public static void main(String[] args) {
+
+        int count = 0, num = 3452;
+
+        while(num != 0)
+        {
+            // num = num/10
+            num /= 10;
+            ++count;
+        }
+
+        System.out.println("Number of digits: " + count);
+    }
+}
